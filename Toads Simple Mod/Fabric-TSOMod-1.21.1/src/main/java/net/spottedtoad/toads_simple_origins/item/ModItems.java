6e22d41f442ca7d236a7feef18153a6e6c7737ce
SpottedToad <deepspacekind@gmail.com>
2026-06-30 15:15:39 -0600

    and textures

--- Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origins/item/ModItems.java
+++ Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origins/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.spottedtoad.toads_simple_origins.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemGroups;
 import net.spottedtoad.toads_simple_origins.TSOMod;
 import net.minecraft.item.Item;
@@ -8,19 +9,38 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-public class ModItems {
-    public static final Item MISERABLE_CREATURE = registerItem("miserable_creature", new Item(new Item.Settings()));
+import static net.minecraft.item.Items.DRIED_KELP;
+import static net.minecraft.item.Items.LEATHER_HORSE_ARMOR;
 
+public class ModItems {
+
+    public static final Item MISERABLE_CREATURE = registerItem("miserable_creature",
+            new Item(new Item.Settings()));
+    public static final Item COOKED_MISERABLE_CREATURE = registerItem("cooked_miserable_creature",
+            new Item(new Item.Settings()));
+
+    public static final Item FISH_BOWL = registerItem("fish_bowl",
+            new ArmorItem(ModArmorMaterials.GLASS_ARMOR_MATERIAL, ArmorItem.Type.HELMET, new Item.Settings()
+                    .maxDamage(ArmorItem.Type.HELMET.getMaxDamage(12))));
+
+    public static final Item REINFORCED_FISH_BOWL = registerItem("reinforced_fish_bowl",
+            new ArmorItem(ModArmorMaterials.GLASS_ARMOR_MATERIAL, ArmorItem.Type.HELMET, new Item.Settings()
+                    .maxDamage(ArmorItem.Type.HELMET.getMaxDamage(20))));
 
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(TSOMod.MOD_ID, name), item);
     }
 
-    public static void registerModItems(){
+    public static void registerModItems() {
         TSOMod.LOGGER.info("Registering Mod Items for " + TSOMod.MOD_ID);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(MISERABLE_CREATURE);
-        });
+            ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+                entries.addAfter(DRIED_KELP, MISERABLE_CREATURE);
+                entries.addAfter(MISERABLE_CREATURE, COOKED_MISERABLE_CREATURE);
+            });
+            ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+                entries.addBefore(LEATHER_HORSE_ARMOR, FISH_BOWL);
+                entries.addAfter(FISH_BOWL, REINFORCED_FISH_BOWL);
+            });
     }
 }
