06b1aa62c7d3d52d2324390f66c05cfd132469a1
SpottedToad <deepspacekind@gmail.com>
2026-07-01 01:04:46 -0600

    fixed the armor

--- Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origins/TSOModClient.java
+++ Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origins/TSOModClient.java
@@ -1,10 +1,15 @@
 package net.spottedtoad.toads_simple_origins;
 
+import mod.azure.azurelib.common.render.armor.AzArmorRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.spottedtoad.toads_simple_origins.item.ModItems;
+import net.spottedtoad.toads_simple_origins.item.armor.FishBowlRenderer;
+import net.spottedtoad.toads_simple_origins.item.custom.FishBowlArmorItem;
 
 public class TSOModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        AzArmorRendererRegistry.register(FishBowlRenderer::new,
+                ModItems.FISH_BOWL);
     }
 }
