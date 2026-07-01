ce46664b455b79f1fb0e0066818498f4a133f77d
SpottedToad <deepspacekind@gmail.com>
2026-07-01 02:12:01 -0600

    updates

--- Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origins/item/armor/FishBowlBoneProvider.java
+++ Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origins/item/armor/FishBowlBoneProvider.java
@@ -9,33 +9,4 @@ public class FishBowlBoneProvider extends AzDefaultArmorBoneProvider {
     public AzBone getHeadBone(AzBakedModel model){
         return model.getBone("armorHead").orElse(null);
     }
-    @Override
-    public AzBone getBodyBone(AzBakedModel model){
-        return model.getBone("armorBody").orElse(null);
-    }
-    @Override
-    public AzBone getRightArmBone(AzBakedModel model){
-        return model.getBone("armorRightArm").orElse(null);
-    }
-    @Override
-    public AzBone getLeftArmBone(AzBakedModel model){
-        return model.getBone("armorLeftArm").orElse(null);
-    }
-    @Override
-    public AzBone getRightLegBone(AzBakedModel model){
-        return model.getBone("armorRightLeg").orElse(null);
-    }
-    @Override
-    public AzBone getLeftLegBone(AzBakedModel model){
-        return model.getBone("armorLeftLeg").orElse(null);
-    }
-    @Override
-    public AzBone getRightBootBone(AzBakedModel model){
-        return model.getBone("armorRightBoot").orElse(null);
-    }
-    @Override
-    public AzBone getLeftBootBone(AzBakedModel model){
-        return model.getBone("armorLeftBoot").orElse(null);
-    }
-
 }
